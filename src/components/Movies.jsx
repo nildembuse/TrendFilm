6d4d5cdce485076c95b24e8adb922d75ec23fd48
Nildem Buse Ertürk <nildembuse@gmail.com>
2024-01-11 21:57:10 +0300

    sayfalar duzenleniyo

---
+++ src/components/Movies.jsx
@@ -0,0 +1,25 @@
+
+
+function Movies () {
+    return(
+        <div className="movies">
+            <h3>Movies</h3>
+            <img src="../assets/img/movies1 .jpg" alt="" />
+            <img src="../assets/img/movies2.jpg" alt="" />
+            <img src="../assets/img/movies3.jpg" alt="" />
+            <img src="../assets/img/movies4 .jpg" alt="" />
+            <img src="../assets/img/movies5.jpg" alt="" />
+            <img src="../assets/img/movies6.jpg" alt="" />
+            <img src="../assets/img/movies7.jpg" alt="" />
+            <img src="../assets/img/movies8.jpg" alt="" />
+            <img src="../assets/img/movies9.jpg" alt="" />
+            <img src="../assets/img/movies10.jpg" alt="" />
+            <img src="../assets/img/movies11.jpg" alt="" />
+            <img src="../assets/img/movise12.jpg" alt="" />
+            <img src="../assets/img/movies13.jpg" alt="" />
+            <img src="../assets/img/movies14.jpg" alt="" />
+            <img src="../assets/img/movies15.jpg" alt="" />
+        </div>
+    )
+}
+export default Movies
---
+++ src/components/Tvseries.jsx
@@ -0,0 +1,23 @@
+
+function Tvseries () {
+    return(
+        <div className="tvseries">
+            <h3>Tvseries</h3>
+            <img src="../assets/img/tvseries1.jpg" alt="" />
+            <img src="../assets/img/tvseries2.jpg" alt="" />
+            <img src="../assets/img/tvseries3.jpg" alt="" />
+            <img src="../assets/img/tvseries4.jpg" alt="" />
+            <img src="../assets/img/tvseries5.jpg" alt="" />
+            <img src="../assets/img/tvseries6.jpg" alt="" />
+            <img src="../assets/img/anamenu.jpg" alt="" />
+            <img src="../assets/img/tvseries8.jpg" alt="" />
+            <img src="../assets/img/tvseries9.jpg" alt="" />
+            <img src="../assets/img/tvseries10.jpg" alt="" />
+            <img src="../assets/img/tvseries11.png" alt="" />
+            <img src="../assets/img/tvseries12.jpg" alt="" />
+            <img src="../assets/img/tvseries13.jpg" alt="" />
+            <img src="../assets/img/tvseries14.jpg" alt="" />
+        </div>
+    )
+}
+export default Tvseries
