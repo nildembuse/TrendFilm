a656d6944f8461243978e600caa4bf3625a3e7ab
Nildem Buse Ertürk <nildembuse@gmail.com>
2024-03-27 16:21:09 +0300

    duzenlemeler basladi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,38 @@
-
+import "/src/App.css"
 function Home () {
     return(
         <div className="anaMenu">
             <h3>Trending</h3>
-            <img src="../assets/img/movies6 .jpg" alt="" />
-            <img src="../assets/img/movies10.jpg" alt="" />
-            <img src="../assets/img/anamenu.jpg" alt="" />
+            <img src="/src/assets/img/movies6 .jpg" alt="" />
+            <img src="/src/assets/img/movies10.jpg" alt="" />
+            <img src="/src/assets/img/anamenu.jpg" alt="" />
 
             <div className="trending">
                 <h4>Recommended for you</h4>
-                <img src="../assets/img/movies1.jpg" alt="" />
-                <img src="../assets/img/tvseries2.jpg" alt="" />
-                <img src="../assets/img/movies2.jpg" alt="" />
-                <img src="../assets/img/movies3.jpg" alt="" />
-                <img src="../assets/img/tvseries14.jpg" alt="" />
-                <img src="../assets/img/tvseries3.jpg" alt="" />
-                <img src="../assets/img/movies4.jpg" alt="" />
-                <img src="../assets/img/tvseries4.jpg" alt="" />
-                <img src="../assets/img/tvseries5.jpg" alt="" />
-                <img src="../assets/img/movies5.jpg" alt="" />
-                <img src="../assets/img/tvseries6.jpg" alt="" />
-                <img src="../assets/img/movies7.jpg" alt="" />
-                <img src="../assets/img/movies8.jpg" alt="" />
-                <img src="../assets/img/movies9.jpg" alt="" />
-                <img src="../assets/img/movies14.jpg" alt="" />
-                <img src="../assets/img/tvseries8.jpg" alt="" />
-                <img src="../assets/img/movies15.jpg" alt="" />
-                <img src="../assets/img/tvseries9.jpg" alt="" />
-                <img src="../assets/img/tvseries10.jpg" alt="" />
-                <img src="../assets/img/tvseries11.jpg" alt="" />
-                <img src="../assets/img/tvseries13.jpg" alt="" />
-                <img src="../assets/img/movies12.jpg" alt="" />
-                <img src="../assets/img/tvseries12.jpg" alt="" />
-                <img src="../assets/img/movies13.jpg" alt="" />
+                <img src="/src/assets/img/movies1.jpg" alt="" />
+                <img src="/src/assets/img/tvseries2.jpg" alt="" />
+                <img src="/src/assets/img/movies2.jpg" alt="" />
+                <img src="/src/assets/img/movies3.jpg" alt="" />
+                <img src="/src/assets/img/tvseries14.jpg" alt="" />
+                <img src="/src/assets/img/tvseries3.jpg" alt="" />
+                <img src="/src/assets/img/movies4.jpg" alt="" />
+                <img src="/src/assets/img/tvseries4.jpg" alt="" />
+                <img src="/src/assets/img/tvseries5.jpg" alt="" />
+                <img src="/src/assets/img/movies5.jpg" alt="" />
+                <img src="/src/assets/img/tvseries6.jpg" alt="" />
+                <img src="/src/assets/img/movies7.jpg" alt="" />
+                <img src="/src/assets/img/movies8.jpg" alt="" />
+                <img src="/src/assets/img/movies9.jpg" alt="" />
+                <img src="/src/assets/img/movies14.jpg" alt="" />
+                <img src="/src/assets/img/tvseries8.jpg" alt="" />
+                <img src="/src/assets/img/movies15.jpg" alt="" />
+                <img src="/src/assets/img/tvseries9.jpg" alt="" />
+                <img src="/src/assets/img/tvseries10.jpg" alt="" />
+                <img src="/src/assets/img/tvseries11.jpg" alt="" />
+                <img src="/src/assets/img/tvseries13.jpg" alt="" />
+                <img src="/src/assets/img/movies12.jpg" alt="" />
+                <img src="/src/assets/img/tvseries12.jpg" alt="" />
+                <img src="/src/assets/img/movies13.jpg" alt="" />
             </div>
 
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './App.css'
 import { createClient } from '@supabase/supabase-js'
 import {RouterProvider ,createBrowserRouter } from 'react-router-dom'
-import Login from './components/Login'
+import Home from './components/Home'
 
 export const supabase = createClient('https://racoxqghzlyvqxcdbswn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhY294cWdoemx5dnF4Y2Ric3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk4NzQ3NzcsImV4cCI6MjAxNTQ1MDc3N30.9EQCgqGVimw_G-LUciAmd6I07p5SiTY_JLGs7MnnO-U')
 
@@ -11,7 +11,7 @@ const router = createBrowserRouter (
   [
     {
       path:"/",
-      element:<Login/>
+      element:<Home/>
     }    
   ]
 )
