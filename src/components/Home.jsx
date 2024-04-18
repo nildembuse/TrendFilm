4fbfe77d781114d122b279b71841b98e53a27dc3
Nildem Buse Ertürk <nildembuse@gmail.com>
2024-04-18 10:36:07 +0300

    datalar duzenleniyor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,30 +14,7 @@ function Home () {
             </div>
             <div className="trending">
                 <h4>Recommended for you</h4>
-                <img src="/src/assets/img/movies1.jpg" alt="" />
-                <img src="/src/assets/img/tvseries2.jpg" alt="" />
-                <img src="/src/assets/img/movies2.jpg" alt="" />
-                <img src="/src/assets/img/movies3.jpg" alt="" />
-                <img src="/src/assets/img/tvseries14.jpg" alt="" />
-                <img src="/src/assets/img/tvseries3.jpg" alt="" />
-                <img src="/src/assets/img/movies4.jpg" alt="" />
-                <img src="/src/assets/img/tvseries4.jpg" alt="" />
-                <img src="/src/assets/img/tvseries5.jpg" alt="" />
-                <img src="/src/assets/img/movies5.jpg" alt="" />
-                <img src="/src/assets/img/tvseries6.jpg" alt="" />
-                <img src="/src/assets/img/movies7.jpg" alt="" />
-                <img src="/src/assets/img/movies8.jpg" alt="" />
-                <img src="/src/assets/img/movies9.jpg" alt="" />
-                <img src="/src/assets/img/movies14.jpg" alt="" />
-                <img src="/src/assets/img/tvseries8.jpg" alt="" />
-                <img src="/src/assets/img/movies15.jpg" alt="" />
-                <img src="/src/assets/img/tvseries9.jpg" alt="" />
-                <img src="/src/assets/img/tvseries10.jpg" alt="" />
-                <img src="/src/assets/img/tvseries11.jpg" alt="" />
-                <img src="/src/assets/img/tvseries13.jpg" alt="" />
-                <img src="/src/assets/img/movies12.jpg" alt="" />
-                <img src="/src/assets/img/tvseries12.jpg" alt=""/>
-                <img src="/src/assets/img/movies13.jpg" alt="" />
+              
             </div>
 
         </div>
